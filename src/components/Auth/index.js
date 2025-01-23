7a42ba91f8df7a85a5b9a6371cf30632ab71b097
steven@afriluck <steven@afriluck.com>
2025-01-23 07:52:38 +0000

    fix

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Notlogged from "./notlogged";
-import LoggedIn from "./loggedin";
-//import { useAuth } from "../../context/AuthContext"; // Import the useAuth hook
+//import Notlogged from "./notlogged";
+//import LoggedIn from "./loggedin";
+import { useAuth } from "../../context/AuthContext";
 
 const Auth = () => {
   const { isLoggedIn, toggleLogin } = useAuth(); // Access the auth context
   // const loggedIn = localStorage.getItem("logged_in");
   return (
     <div>
-      {loggedIn ? <LoggedIn /> : <Notlogged />}
+      {/* {isLoggedIn ? <LoggedIn /> : <Notlogged />} */}
       {<button onClick={toggleLogin}>{isLoggedIn ? "Log Out" : "Log In"}</button>}
     </div>
   );
