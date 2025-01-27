7fafe8be6b33911428a40b6846deaca515b28b87
steven@afriluck <steven@afriluck.com>
2025-01-27 09:35:05 +0000

    fix

--- src/index.js
+++ src/index.js
@@ -12,18 +12,18 @@ import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log("ServiceWorker registered with scope:", registration.scope);
-      })
-      .catch((error) => {
-        console.error("ServiceWorker registration failed:", error);
-      });
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/service-worker.js")
+//       .then((registration) => {
+//         console.log("ServiceWorker registered with scope:", registration.scope);
+//       })
+//       .catch((error) => {
+//         console.error("ServiceWorker registration failed:", error);
+//       });
+//   });
+// }
 root.render(
   <React.StrictMode>
     <Provider store={store}>
