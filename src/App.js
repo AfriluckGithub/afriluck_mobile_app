515a031db072d92acfd5a42858edff93f93d77fb
steven@afriluck <steven@afriluck.com>
2025-01-27 11:42:27 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { useEffect } from "react";
+//import { useEffect } from "react";
 import "preline/preline";
-import { useLocation } from "react-router-dom";
+//import { useLocation } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import "./output.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
 function App() {
-  const location = useLocation();
-  useEffect(() => {
-    window.HSStaticMethods.autoInit();
-  }, [location.pathname]);
+  // const location = useLocation();
+  // useEffect(() => {
+  //   window.HSStaticMethods.autoInit();
+  // }, [location.pathname]);
 
   return (
     <div className="bg-[#F7F7F7] h-screen flex flex-col">
