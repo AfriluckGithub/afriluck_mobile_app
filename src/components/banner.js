7e3b3aab05bf60a03f7f8f0aa145e2bf1665dfb7
steven@afriluck <steven@afriluck.com>
2025-01-29 20:44:25 +0000

    fix

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,7 +1,7 @@
 const Banner = ({image}) => {
     return (
         <div className="m-5">
-            <img src={image} alt="Afriluck Lotto" class="w-full h-48 object-contain" />
+            <img src={image} alt="Afriluck Lotto" class="w-full h-auto object-fill rounded-lg" />
         </div>
     );
 };
