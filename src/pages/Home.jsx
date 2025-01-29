735d751ee4f6696dcb4ca89772860f0b2bbfc3d1
steven@afriluck <steven@afriluck.com>
2025-01-29 20:39:00 +0000

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
   ];
 
   return (
-    <div className="h-[130vh]">
+    <div className="h-[150vh]">
       <SearchBar query={query} setQuery={setQuery} />
       <Banner image={"banner.png"} />
       <Content
