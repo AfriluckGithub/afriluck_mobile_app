c51f5031fabd5eb4f180a456413e784d28af2b1a
steven@afriluck <steven@afriluck.com>
2025-02-03 10:07:03 +0000

    fix

--- src/components/game.js
+++ src/components/game.js
@@ -17,9 +17,9 @@ const Game = ({ title, image, subtitle, type }) => {
       className="flex flex-col gap-4 m-1 bg-gray-100 rounded-xl p-2 mt-5 w-24 h-24 justify-center items-center hover:bg-green-100 focus:bg-green-100"
     >
       <img alt="logo" className="flex flex-row h-10 w-20" src={image} />
-      <p className="font-normal text-center text-wrap text-xs text-black">
-        {/* {subtitle} */}
-      </p>
+      {/* <p className="font-normal text-center text-wrap text-xs text-black">
+        {subtitle} 
+      </p> */}
     </div>
   );
 };
