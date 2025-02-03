a5f89f4c9216bb877fb556ffca9b88fb4e324668
steven@afriluck <steven@afriluck.com>
2025-02-03 10:38:44 +0000

    fix

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../output.css";
 
 const Header = () => {
@@ -35,21 +35,25 @@ const Header = () => {
   return (
     <div className="flex flex-row justify-between items-center w-full h-auto bg-[#F7F7F7] fixed py-6  top-0 left-0 right-0 z-50">
       {/* Conditionally render the KF div */}
-      {location.pathname !== "/profile" && (
-        <div className="ml-6">
-          <div
-            style={{ backgroundColor: "#156064" }}
-            className="flex flex-wrap h-12 w-auto text-center text-white rounded-full font-semibold justify-center items-center"
-          >
-            <p className="flex justify-center items-center p-3">KF</p>
+      <Link to={"/profile"}>
+        {location.pathname !== "/profile" && (
+          <div className="ml-6">
+            <div
+              style={{ backgroundColor: "#156064" }}
+              className="flex flex-wrap h-12 w-auto text-center text-white rounded-full font-semibold justify-center items-center"
+            >
+              <p className="flex justify-center items-center p-3">KF</p>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </Link>
       <div className="flex flex-wrap w-full text-center font-medium text-xl justify-center items-center">
         <p>{getTitle()}</p>
       </div>
       <div className="flex flex-wrap w-auto justify-center items-center mr-6">
-        <p className="flex flex-wrap w-12 h-12 justify-center items-center">{getIcon()}</p>
+        <p className="flex flex-wrap w-12 h-12 justify-center items-center">
+          {getIcon()}
+        </p>
       </div>
     </div>
   );
