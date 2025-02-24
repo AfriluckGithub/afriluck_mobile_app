5a730e850f0f0d59b954c981ae75022a8f83f326
steven@afriluck <steven@afriluck.com>
2025-02-24 07:45:35 +0000

    fix

--- src/pages/Mybet.jsx
+++ src/pages/Mybet.jsx
@@ -1,14 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useMemo, useState } from "react";
-//import SearchBar from "../components/myBetSearchbar";
 import { OrbitProgress } from "react-loading-indicators";
 import { useSelector } from "react-redux";
 
 const Mybet = () => {
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-  //const user = useSelector((state) => state.user.user);
   const user = useSelector((state) => state.user?.user);
 
   const memoizedUser = useMemo(() => {
@@ -16,109 +13,106 @@ const Mybet = () => {
   }, [user]);
 
   useEffect(() => {
-    try {
-      var token = null;
-      if (memoizedUser) {
-        token = memoizedUser.token;
-      }
-      const getMyBets = async () => {
-        setLoading(true);
-        try {
-          const res = await axios.get(
-            "https://app.afriluck.com/api/V1/app/my-bets",
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json",
-              },
-            }
-          );
-          setLoading(false);
-          console.log(res);
-
-          if (res.status === 200) {
-            setResults(res.data.success);
-          } else if (res.status === 401) {
-            setError("Please log in to see your ticket info");
-          }
-        } catch (e) {
-          setError("Please log in to see your ticket info");
-          setLoading(false);
-          console.log(e);
-        }
-      };
-      getMyBets();
-    } catch (error) {
+    
+    const getMyBets = async () => {
+      setLoading(true);
       try {
+        const token = memoizedUser?.token || null;
+        const res = await axios.get(
+          "https://app.afriluck.com/api/V1/app/my-bets",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
         setLoading(false);
-        console.log(error);
-        setError("Please log in to see your ticket info");
-      } catch (error) {
-        console.log(error);
+
+        console.log(res.data);
+        
+        
+        if (res.status === 200) {
+          setResults(res.data.success);
+        } else if (res.status === 401) {
+          setLoading(false);
+        }
+      } catch (e) {
+        setLoading(false);
       }
+    };
+    
+    if (memoizedUser) {
+      getMyBets();
     }
   }, [memoizedUser]);
-  console.log("Error => ", error);
+
+  const groupedResults = results.reduce((acc, result) => {
+    if (!acc[result.date]) {
+      acc[result.date] = [];
+    }
+    acc[result.date].push(result);
+    return acc;
+  }, {});
+
+  
 
   return (
-    <>
-      <div className="flex flex-col bg-[#F7F7F7] mx-4 md:mx-12 lg:mx-48">
-        {/* <div className="">
-          <SearchBar />
-        </div> */}
-        <div className="flex flex-col w-full my-32 ">
-          {results.map((result) => (
-            <div className="bg-white rounded-xl h-auto w-full p-6 my-6 border border-border-default ">
-              <div className="flex flex-row w-auto space-x-4 pb-4">
-                <span>
-                  <img alt="afriluck" src="afriluck_lg.png" />
-                </span>
-                <div className="flex flex-col items-start font-semibold text-gray-600 space-y-1 font-sans text-wrap w-full ">
-                  <div className="flex flex-row text-base">
-                    <div className="mr-2 font-medium font-Poppins">
-                      {result.draw_code},
-                    </div>
-                    <div className="font-Poppins">{result.date}</div>
-                  </div>
-                  <div className="text-gray-400 font-Poppins ">
-                    {result.selected_numbers}
-                  </div>
-                </div>
-              </div>
-              <hr />
-              <div className="mt-2">
-                <div class="flex justify-between w-full">
-                  <div className="flex flex-row">
-                    <div className="mr-2">
-                      <img alt="share" src={"share.svg"} />{" "}
-                    </div>
-                    <div> Share</div>
-                  </div>
-                  <span>{result.time}</span>
-                </div>
-              </div>
+    <div className="flex flex-col bg-[#F7F7F7] mx-4 md:mx-12 lg:mx-48">
+      {!memoizedUser && (
+        <p className="flex justify-center items-center h-screen w-screen text-wrap text-center text-black">
+          {"Please log in to see your ticket info"}
+        </p>
+      )}
+      
+      <div className="flex flex-col w-full my-32">
+        {Object.keys(groupedResults).map((date) => (
+          <div key={date}>
+            <div className="font-Poppins text-lg font-normal text-white px-6 py-2 bg-[#156064] rounded-md">
+            {date}
             </div>
-          ))}
-          {error && (
-            <p className="flex justify-center items-center h-screen text-wrap p-5 text-center text-black">
-              {error}
-            </p>
-          )}
+            {groupedResults[date].map((result, index) => (
+              <div key={index} className="bg-white rounded-xl h-auto w-full p-6 my-6 border border-border-default">
+                <div className="flex flex-row w-auto space-x-4 pb-4">
+                  <span>
+                    <img alt="afriluck" src="afriluck_lg.png" />
+                  </span>
+                  <div className="flex flex-col items-start font-semibold text-gray-600 space-y-1 font-sans text-wrap w-full">
+                    <div className="flex flex-row text-base">
+                      <div className="mr-2 font-medium font-Poppins">
+                        {result.draw_code},
+                      </div>
+                      <div className="font-Poppins">{result.date}</div>
+                    </div>
+                    <div className="text-gray-400 font-Poppins">
+                      {result.selected_numbers}
+                    </div>
+                  </div>
+                </div>
+                <hr />
+                <div className="mt-2">
+                  <div className="flex justify-between w-full">
+                    <div className="flex flex-row">
+                      <div className="mr-2">
+                        <img alt="share" src={"share.svg"} />
+                      </div>
+                      <div>Share</div>
+                    </div>
+                    <span>{result.time}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+
+        {loading && (
           <div className="flex justify-center items-center h-screen">
-          {loading ? (
-            <OrbitProgress
-              color="#000"
-              size="small"
-              text="loading"
-              textColor=""
-            />
-          ) : (
-            <p></p>
-          )}
-        </div>
-        </div>
+            <OrbitProgress color="#000" size="small" text="loading" textColor="" />
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
