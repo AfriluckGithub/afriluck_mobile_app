bfe2c6a412f9e833b06364cdda5f5115088b8180
steven@afriluck <steven@afriluck.com>
2025-03-29 19:44:25 +0000

    fix

--- src/components/content.js
+++ src/components/content.js
@@ -99,15 +99,15 @@ const Body = ({ subGames, subGames1 }) => {
     {
       name: "Anopa",
       timeLeft: timeLeft.Anopa,
-      started: isGameActive(19, 45, 10, 0, "Anopa"),
-      //started: true,
+      //started: isGameActive(19, 45, 10, 0, "Anopa"),
+      started: true,
       games: subGames1,
     },
     {
       name: "Midday",
       timeLeft: timeLeft.Midday,
-      started: isGameActive(19, 45, 13, 30, "Midday"),
-      //started: true,
+      //started: isGameActive(19, 45, 13, 30, "Midday"),
+      started: true,
       games: subGames1,
     },
     {
