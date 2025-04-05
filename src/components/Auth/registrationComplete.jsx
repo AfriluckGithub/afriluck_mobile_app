3b241c429e18ab80130bb5b4a0396bd9193e983c
steven@afriluck <steven@afriluck.com>
2025-04-05 13:41:03 +0000

    fix

--- src/components/Auth/registrationComplete.jsx
+++ src/components/Auth/registrationComplete.jsx
@@ -32,10 +32,10 @@ export default function RegistrationComplete() {
           Thank you for signing up. You can now access your account.
         </p>
         <Button
+          label={"Go to Home Page"}
           className="mt-6 px-6 py-2 bg-primary text-white rounded-lg shadow-md hover:bg-primary-600 transition"
           onClick={loginPage}
         >
-          Go to Home Page
         </Button>
       </div>
     </div>
