78b26b269104ec7b90267918fe63803d6dc78c21
steven@afriluck <steven@afriluck.com>
2025-04-11 12:36:00 +0000

    fix

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,22 +1,55 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
 import Logout from "../components/logout";
 import Accounts from "../components/Accounts";
 import Privacy from "../components/Privacy";
 import Share from "../components/Share";
 import Auth from "../components/Auth";
-import { useSelector } from "react-redux";
-
-
+import { useSelector, useDispatch } from "react-redux";
+import { update } from "../store/userSlice";
 
 const Profile = () => {
   const user = useSelector((state) => state.user?.user);
+  const dispatch = useDispatch();
   const memoizedUser = useMemo(() => {
     return user ? { ...user } : null;
   }, [user]);
   window.scrollTo(0, 0);
-  const containerHeight = memoizedUser ? "lg:h-[1200px] sm:h-[800px]" : "lg:h-[800px] sm:h-[800px]";
+  const containerHeight = memoizedUser
+    ? "lg:h-[1200px] sm:h-[800px]"
+    : "lg:h-[800px] sm:h-[800px]";
+
+  useEffect(() => {
+    const getBalance = async () => {
+      try {
+        const response = await fetch(
+          "https://app.afriluck.com/api/V1/app/account/balance",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${memoizedUser?.token}`,
+            },
+          }
+        );
+        const json = await response.json();
+        if (response.status === 200) {
+          console.log("Balance => ", json.balance);
+          dispatch(update({ balance: json.balance }));
+        } else if (response.status === 401) {
+          console.log("Session expired, please login again");
+        } else {
+          console.log("Error fetching balance");
+        }
+      } catch (error) {
+        console.error("Error fetching balance:", error);
+      }
+    };
+    getBalance();
+  }, [dispatch, memoizedUser]);
   return (
-    <div className={`flex flex-col ${containerHeight} sm:h-[800px] my-32 space-y-6 mx-4 md:mx-12 lg:mx-48`}>
+    <div
+      className={`flex flex-col ${containerHeight} sm:h-[800px] my-32 space-y-6 mx-4 md:mx-12 lg:mx-48`}
+    >
       <div className="space-y-6">
         <div className="space-y-4 ">
           <Auth />
