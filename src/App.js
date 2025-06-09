32dede51c11fb701e1e60e1d201d20f9abe7760f
steven@afriluck <steven@afriluck.com>
2025-06-09 16:08:18 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //import { useEffect } from "react";
 import "preline/preline";
 //import { useLocation } from "react-router-dom";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 //import Header from "./components/header";
 //import Footer from "./components/footer";
 import PWAInstallPrompt from "./PWAInstallPrompt";
@@ -9,6 +9,7 @@ import { useAptabase } from "@aptabase/react";
 import { useEffect } from "react";
 import { Suspense, lazy } from "react";
 import TagManager from "react-gtm-module";
+import * as Sentry from "@sentry/react";
 
 const tagManagerArgs = {
   gtmId: "GTM-5Q9QP597",
@@ -20,12 +21,25 @@ const Footer = lazy(() => import("./components/footer"));
 function App() {
   const { trackEvent } = useAptabase();
 
-  // const location = useLocation();
+  const location = useLocation();
   useEffect(() => {
     //window.HSStaticMethods.autoInit();
     //trackEvent("started_app");
+    try {
+      Sentry.setUser({
+        ip_address: location.ip,
+        geo: {
+          country_code: location.countryCode,
+          region: location.region,
+          city: location.city,
+        },
+        
+      });
+    } catch (error) {
+      console.error("Error initializing HSStaticMethods:", error);
+    }
     TagManager.initialize(tagManagerArgs);
-  }, [trackEvent]);
+  }, [trackEvent, location]);
 
   return (
     <div className="bg-[#F7F7F7] h-screen flex flex-col">
